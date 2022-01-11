3a52452be9136fa58ac5edf5c90ff255cc9847bf
Andrea Franceschini <andrea.franceschini03@gmail.com>
2022-01-11 11:54:09 +0100

    ex 1.10

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -4,23 +4,32 @@ const Button = (props) => (
   <button onClick={props.handleClick}>{props.text}</button>
 )
 
+const StatisticLine = (props) => {
+  return (
+    <p>{props.text} {props.value}</p>
+    )
+}
+
 const Statistics = ({good, neutral, bad}) => {
   
   const total = good + neutral + bad
   const average = ( good - bad ) / total
   const positive = ( good / total ) * 100
-  return (
-  <>
-    <h1>Statistics</h1>
 
-    <p>good {good}</p>
-    <p>neutral {neutral}</p>
-    <p>bad {bad}</p>
-    <p>all {total}</p>
-    <p>average {average}</p>
-    <p>positive {positive} %</p>
+  return total ? (
+  <>
+    <StatisticLine text="good" value={good}/>
+    <StatisticLine text="neutral" value={neutral}/>
+    <StatisticLine text="bad" value={bad}/>
+    <StatisticLine text="all" value={total}/>
+    <StatisticLine text="average" value={average}/>
+    <StatisticLine text="positive" value={positive}/>
   </>
-)}
+  ) : (
+    <>
+      <p>No feedback given</p>
+    </>
+  )}
 
 const App = () => {
   // save clicks of each button to its own state
@@ -52,7 +61,8 @@ const App = () => {
       <Button handleClick={() => handleClick("good")} text="good"/>
       <Button handleClick={() => handleClick("neutral")} text="neutral"/>
       <Button handleClick={() => handleClick("bad")} text="bad"/>
-
+      
+      <h1>Statistics</h1>
       <Statistics 
         good={good}
         neutral={neutral}
