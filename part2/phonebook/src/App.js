e4f2da08ae32e30dae452ca80195ac4d64de4d01
Andrea Franceschini <andrea.franceschini03@gmail.com>
2022-01-12 23:47:50 +0100

    ex 2.8

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -2,36 +2,54 @@ import React, { useState } from 'react'
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas' }
+    { 
+      name: 'Arto Hellas',
+      number: "555-555-555"
+    }
   ]) 
-  const [newName, setNewName] = useState('')
 
-  const handleNewNameChange = (event) => {
-    const {value} = event.target
+  const [formData, setFormData] = useState({
+    name: "",
+    number: ""
+  })
 
-    setNewName(value)
+  const handleFormChange = (event) => {
+    const {value, name} = event.target
+
+    setFormData(prev => ({
+      ...prev, 
+      [name]: value
+    }))
   }
 
   const handleSubmit = (event) => {
     event.preventDefault() 
 
-    if (persons.filter(person => person.name === newName).length) {
-      window.alert(`${newName} is already saved in your contacts!`)
-      setNewName("")
+    if (persons.filter(person => person.name === formData.name).length) {
+      window.alert(`${formData.name} is already saved in your contacts!`)
+      setFormData({
+        name: "",
+        number: ""
+      })
       return
     }
 
     const newPerson = {
-      name: newName
+      name: formData.name,
+      number: formData.number
     }
     setPersons(prev => ([
       ...prev,
       newPerson
     ]))
-    setNewName("")
+
+    setFormData({
+      name: "",
+      number: ""
+    })
   }
 
-  const numbersEl = persons.map(person => <p key={person.name}>{person.name}</p>)
+  const numbersEl = persons.map(person => <p key={person.name}>{person.name} - {person.number}</p>)
 
   return (
     <div>
@@ -39,10 +57,21 @@ const App = () => {
       <form onSubmit={handleSubmit}>
         <div>
           name: <input 
-                  value={newName} 
+                  name='name'
+                  value={formData.name} 
                   placeholder='New Name' 
-                  onChange={handleNewNameChange}/>
+                  onChange={handleFormChange}
+                  />
         </div>
+        <div>
+          number: <input 
+                      name='number'
+                      value={formData.number} 
+                      placeholder='Phone Number' 
+                      onChange={handleFormChange}/>
+        
+        </div>
+
         <div>
           <button type="submit">add</button>
         </div>
